576d3e3ce8f0a8d7cac69697fc7e320cfa187a17
Gaurav Vasudev <gaurav.nv1@gmail.com>
2019-10-08 21:17:20 -0700

    Added Procfile

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Beautiful :)
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://img.picturequotes.com/2/3/2523/you-are-important-quote-1.jpg"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Glad to see you here
         </a>
       </header>
     </div>
