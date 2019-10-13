d5664b62f00eb8f0c149c3669c0c462a80526e55
Gaurav Vasudev <gaurav.nv1@gmail.com>
2019-10-12 18:44:03 -0700

    Placeholder message added

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        This is a placeholder website which will be updated soon!
         <p>
-          Hello Beautiful :)
+          Hello there, you beautiful human being :)
         </p>
         <a
           className="App-link"
