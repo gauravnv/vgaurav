7acce9571a183c462c69a54a0cba47ed6d8a9f95
Gaurav Vasudev <gaurav.nv1@gmail.com>
2019-10-14 12:46:17 -0700

    Temporary Page and Title Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        This is a placeholder website which will be updated soon!
+        <font size="1">This is a placeholder website which will be updated soon!</font>
         <p>
           Hello there, you beautiful human being :)
         </p>
